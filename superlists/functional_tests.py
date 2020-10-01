7b9451c31f9650f11a233ff0fe4c9c2b9145ed7f
joseph <joseph@joseph-ThinkPad-X1-Extreme>
2020-10-01 20:38:24 +0800

    處理selenium 遇到的StaleElementReferenceException並達到想要的測試結果

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import  Keys
 import unittest
 from selenium.common.exceptions import StaleElementReferenceException
+from superlists.tools import tool
 
 class NewVisitorTest(unittest.TestCase):
     
@@ -11,6 +12,14 @@ class NewVisitorTest(unittest.TestCase):
         
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        #self.browser.refresh()
+        #self.browser.get("http://localhost:8000")
+        with(tool.wait_page_load(self.browser)):
+            table = self.browser.find_element_by_id("id_list_table")
+            rows = table.find_elements_by_tag_name("tr")
+            self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
@@ -29,13 +38,18 @@ class NewVisitorTest(unittest.TestCase):
         
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
         
-        self.browser.refresh()
-        self.browser.get("http://localhost:8000")
+        #self.browser.refresh()
+        #self.browser.get("http://localhost:8000")
         #此時仍有一個文字方塊，讓他可以加入令一個項目
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
+        
+        #網頁更新，此時他的清單有這兩項
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers make a fly")
         
         #table = self.browser.find_element_by_id("id_list_table")
         #rows = table.find_element_by_tag_name("tr")
---
+++ superlists/superlists/tools/tool.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support.expected_conditions import staleness_of
+
+
+class wait_page_load:
+    def __init__(self, browser, timeout=5):
+        self.browser = browser
+        self.timeout = timeout
+        
+    def __enter__(self):
+        self.old_page = self.browser.find_element_by_tag_name('html')
+    
+    def __exit__(self, *_):
+        WebDriverWait(self.browser, self.timeout).until(staleness_of(self.old_page))
