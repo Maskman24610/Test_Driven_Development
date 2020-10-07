cefeeeb7ec4e514b8c0acd28d762a839aa1fc162
joseph <joseph@joseph-ThinkPad-X1-Extreme>
2020-10-07 19:39:08 +0800

    修敢新增functional_tests/test.py

--- superlists/superlists/functional_tests/tests.py
+++ superlists/superlists/functional_tests/tests.py
@@ -38,6 +38,8 @@ class NewVisitorTest(LiveServerTestCase):
             )
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/list/.+")
         #with(tool.wait_page_load(self.browser)):
         time.sleep(1)
         self.check_for_row_in_list_table("1: Buy peacock feathers")
@@ -54,12 +56,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
         
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn(
-                "2: Use peacock feathers to make a fly",
-                [row.text for row in rows]
-                )
+        #現在有一個新的使用者Francis 來到網站
+        ##我們使用一個新的瀏覽器殲段工作來確保
+        ##前一個使用者的任何資訊都不會被cookies等機制送出
+        self.browser.quit()
+        self.browser = webdriver.Firefox(executable_path="/home/joseph/git/Test_Driven_Development/superlists/geckodriver")
+        
+        #Francis造訪首頁，並沒有出現前一個使用者的清單的跡象
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+        
+        #Francis 輸入一個新項目，做出一個新的清單
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        
+        #Francis取捯他自己獨一無二的清單
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/+.")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
         
         self.fail("Finish the test!")
