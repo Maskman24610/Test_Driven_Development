e525ef86780d4f0e27db947b136a30d0b9c00120
joseph <joseph@joseph-ThinkPad-X1-Extreme>
2020-10-11 14:46:56 +0800

    first step of FT for layout + stying

--- superlists/superlists/functional_tests/tests.py
+++ superlists/superlists/functional_tests/tests.py
@@ -105,3 +105,25 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn("Buy milk", page_text)
         
         #self.fail("Finish the test!")
+    def test_layout_and_stying(self):
+        #前往首頁
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        #開始編輯新清單，看到這裡的
+        #輸入方塊也妥善的置中
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta = 5,
+            )
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
